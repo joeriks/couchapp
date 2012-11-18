540fc0e606a922fabb764e7b7c965bb612ed09bd
joeriks <jonas@programenta.se>
2012-11-19 00:40:30 +0100

    demo

---
+++ app.js
@@ -0,0 +1,25 @@
+
+function opacity(pct) {
+    return {
+        opacity: String(pct / 100),
+        filter: "alpha(opacity=" + pct + ")"
+    };
+}
+HTML({
+    lang: "en"
+}, HEAD(TITLE("Welcome to domo"), STYLE({
+    type: "text/css"
+}, STYLE.on("body", {
+    textAlign: "center",
+    fontSize: 50
+}), STYLE.on("h1", opacity(50), {
+    background: "#000",
+    color: "#fff"
+}))), BODY(H1("Welcome to domo")));
+$.couch.allDbs({
+    success: function (dbs) {
+        dbs.forEach(function (db) {
+            $("#databases").append('<li><a href="/_utils/database.html?' + db + '">' + db + '</a></li>');
+        });
+    }
+});
---
+++ app.ts
@@ -0,0 +1,26 @@
+﻿declare var $, HTML, HEAD, TITLE, STYLE, BODY, H1;
+
+function opacity(pct) {
+    return {opacity: String(pct / 100), filter: "alpha(opacity=" + pct + ")"}
+  }
+
+  HTML({lang: "en"},
+    HEAD(
+      TITLE("Welcome to domo"),
+      STYLE({type: "text/css"},
+        STYLE.on("body", {textAlign: "center", fontSize: 50}),
+        STYLE.on("h1", opacity(50), {background: "#000", color: "#fff"})
+      )
+    ),
+
+    BODY(H1("Welcome to domo"))
+  )
+
+
+  $.couch.allDbs({
+      success : function(dbs) {
+        dbs.forEach(function(db) {
+          $("#databases").append('<li><a href="/_utils/database.html?'+db+'">'+db+'</a></li>');
+        });
+      }
+    });
